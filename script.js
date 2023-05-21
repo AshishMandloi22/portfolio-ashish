28f2c12ecefa19b7c17578355a82e82dbdea39e6
Ashish_Mandloi <112812220+AshishMandloi22@users.noreply.github.com>
2023-05-21 12:49:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/*typing animation */
+var typed = new Typed(".typing",{
+    strings:["","web developer","web designer","web developer","web deigner"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
